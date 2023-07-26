d30198067da95343c4cc2de63f8b8fcfab67d0aa
seasea128 <seasea@windowslive.com>
2023-07-26 12:19:07 +0700

    add more language support

--- init.el
+++ init.el
@@ -120,7 +120,7 @@
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       ;;csharp            ; unity, .NET, and mono shenanigans
+       (csharp +lsp +tree-sitter +dotnet)            ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
        (dart +flutter +lsp)   ; paint ui and not much else
        ;;dhall
@@ -153,7 +153,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pretty)               ; organize your plain life in plain text
+       (org +pretty +gnuplot +dragndrop)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
